a962fb5c31b2955739ed0206fcbdc508a49fb194
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2024-06-25 08:35:14 +0200

    added some nonsense

--- Concepts/Program.cs
+++ Concepts/Program.cs
@@ -1,2 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
-// print("Hello, World!") # Python
+﻿int[] numbers = { 1, 2, 3, 4, 5 };
+
+NumbersArray numbersArray = new NumbersArray(numbers);
+
+Console.WriteLine(numbersArray);
+
+// Mein Number Array Datentyp
+public class NumbersArray
+{
+  public int[] numbers = [];
+
+  public NumbersArray(int[] numbers)
+  {
+    this.numbers = numbers;
+  }
+
+  // Überschreibe die ToString Methode, weil C# sonst nicht weiß, wie es das Objekt ausgeben soll
+  public override string ToString()
+  {
+    return string.Join(", ", numbers);
+  }
+}
