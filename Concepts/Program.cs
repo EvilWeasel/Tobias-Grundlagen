a087894aa2a4214fef091a5fa55e3d9b0789fcb9
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2024-06-25 09:34:14 +0200

    Frühstückspause

--- Concepts/Program.cs
+++ Concepts/Program.cs
@@ -1,22 +1,39 @@
-﻿int[] numbers = { 1, 2, 3, 4, 5 };
+﻿string name = "tobi";
+/*
+ PascalCase => WorttrennungenWerdenGrossGeschrieben
+  camelCase => worttrennungenWerdenKleingeschrieben
+  snake_case => worttrennungen_werden_kleingeschrieben
+  kebab-case => worttrennungen-werden-kleingeschrieben
+ */
 
-NumbersArray numbersArray = new NumbersArray(numbers);
+/*
+ DateTime ist einer von vielen komplexen Datentypen in C#.
+  DateTime ist ein Strukturtyp, der Datum und Uhrzeit darstellt.
+  Komplexe DatenTypen sind Objekte, die aus mehreren Werten bestehen, welche
+    ebenfalls komplex oder auch primitv sein können.
+   Objekte können nur in der Konsole ausgegeben werden, wenn sie eine Methode
+    ToString() haben.
+ */
+DateTime date = DateTime.Now;
+Console.WriteLine(date.Year);
 
-Console.WriteLine(numbersArray);
+Console.WriteLine(name);
 
-// Mein Number Array Datentyp
-public class NumbersArray
-{
-  public int[] numbers = [];
 
-  public NumbersArray(int[] numbers)
-  {
-    this.numbers = numbers;
-  }
 
-  // Überschreibe die ToString Methode, weil C# sonst nicht weiß, wie es das Objekt ausgeben soll
-  public override string ToString()
-  {
-    return string.Join(", ", numbers);
-  }
-}
+// Funktionen
+// x = 5y
+var x = Console.WriteLine("buxtehude");
+var y = Console.ReadLine();
+
+var x = name.Trim();
+
+//Console.WriteLine(x);
+
+var test = 5.5;
+
+// implicit conversion
+Console.WriteLine((int)test + (int)test);
+
+// explicit conversion
+int num = int.Parse(test);
