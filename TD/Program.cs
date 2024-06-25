905f2f8409c73bca1f14a81fa254da2ce1ef39ec
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2024-06-25 14:01:54 +0200

    added todo-app diagram

--- TD/Program.cs
+++ TD/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace TD
 {
+  // Programmeinstiegspunkt (Wo das Programm anfängt auszuführen.)
+  // => Program.Main
   internal class Program
   {
     static void Main(string[] args)
     {
       Console.WriteLine("Hello, World!");
+      Console.ReadLine();
+      // hier...
+      var x = Start();
+      Console.WriteLine(x);
     }
+    static string Start()
+    {
+      Console.WriteLine("Buxtehude!");
+      return "blubb";
+    }
+    // const login = function() {...}
+    // login()
   }
 }
