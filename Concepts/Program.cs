eb8401fb014fcfeee780730936ea544f38107292
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2024-06-25 15:48:23 +0200

    Menu for todo-app

--- Concepts/Program.cs
+++ Concepts/Program.cs
@@ -67,3 +67,23 @@ string GetGreeting()
 var greeting =  GetGreeting();
 
 Console.WriteLine(greeting);
+
+// Chararrays
+var my_array = "Feierabend!";
+var my_letter = my_array[0];
+
+// Collections-Basics
+// Array
+string[] namen = new string[5];
+string[] alternative_namen = ["Tobi", "Popstar", "Lehrer"]; // max 3 items
+string[] alternative_namen1 = { "Ab", "Bc", "Cd" };
+namen[0] = "Tobi";
+namen[1] = "Melanie";
+namen[2] = "Torben";
+namen[99999999999999998] = "Buxtehude!";
+Console.WriteLine(namen[0][0]);
+
+
+// Array ist eine Liste an Werten
+// Eine kontinuirliche Speicherregion, für mehrere Daten eines gewissen Types
+int[] nums = new int[5];
--- TD/Program.cs
+++ TD/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace TD
+﻿/* Program-Einstiegspunkt C#
+ * namespace TD
 {
   // Programmeinstiegspunkt (Wo das Programm anfängt auszuführen.)
   // => Program.Main
@@ -21,3 +22,32 @@
     // login()
   }
 }
+*/
+
+// Menü anzeigen mit Optionen
+Console.WriteLine("Willkommen bei TD!");
+Console.WriteLine("Was möchtest du tun?");
+Console.WriteLine("\t1. Neues Todo erstellen\n\t2. Todos anzeigen");
+
+do
+{
+  var input = Console.ReadLine();
+
+  switch (input)
+  {
+    case "1":
+      // Option 1: Todo erstellen
+      // Userintput nehmen
+      var todo = Console.ReadLine();
+      
+      // Neues Todo zur Todoliste hinzufügen
+      // Todo erstellen
+      Console.WriteLine("CREATE");
+      break;
+    case "2":
+      // Liste der Todos anzeigen
+      Console.WriteLine("READ");
+      break;
+  }
+} while (false);
+
