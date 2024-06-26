4b4760aa063050be041e17cabb892a98eecdc578
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2024-06-26 12:01:54 +0200

    Implemented adding, listing and removing of todos

--- Concepts/Program.cs
+++ Concepts/Program.cs
@@ -80,10 +80,51 @@ string[] alternative_namen1 = { "Ab", "Bc", "Cd" };
 namen[0] = "Tobi";
 namen[1] = "Melanie";
 namen[2] = "Torben";
-namen[99999999999999998] = "Buxtehude!";
+// namen[99999999999999998] = "Buxtehude!";
 Console.WriteLine(namen[0][0]);
+
+int increment_me = 0;
+// Die folgenden 2 Zeilen sind syntaktisch gleich
+increment_me = increment_me + 1;
+increment_me += 1;
+// increment-operator
+increment_me++;
+Console.WriteLine("Increment me is: ");
+Console.WriteLine(increment_me);
+// String-Interpolation
+// Hat einige Nachteile, was Sicherheit angeht (s. Vertiefung)
+Console.WriteLine($"Increment me is: {increment_me}");
 
 
 // Array ist eine Liste an Werten
 // Eine kontinuirliche Speicherregion, für mehrere Daten eines gewissen Types
+// Ein Array hat immer eine feste Größe, welche bei der Initialisierung festgelegt werden muss
+// Array.Length ist immer die maximale Länge des Arrays
 int[] nums = new int[5];
+nums[0] = 10;
+/*
+ For Loop - 4 Komponenten
+   1. Deklaration der Zählervariablen
+   2. Bedingung der Schleife (Wann zu Ende?)
+   3. Operation, welche nach jeder Iteration ausgeführt wird
+      -> idr. Inkrement (i++) oder Dekrement (i--)
+   4. Codeblock, welcher definiert, was bei jeder iteration der Schleife passieren soll.
+ */
+for (int i = 0; i < nums.Length; i++)
+{
+  Console.WriteLine(nums[i]);
+}
+
+int test_me = 0;
+
+// Variablen, welche innerhalb eines Blocks deklariert sind,
+//   können nicht außerhalb dieses Blocks verwendet werden.
+{
+  Console.WriteLine(test_me);
+  int my_cool_variable = 0;
+  Console.WriteLine(my_cool_variable);
+  { 
+    Console.WriteLine(my_cool_variable);
+  }
+}
+Console.WriteLine(my_cool_variable);
--- TD/Program.cs
+++ TD/Program.cs
@@ -26,28 +26,64 @@
 
 // Menü anzeigen mit Optionen
 Console.WriteLine("Willkommen bei TD!");
-Console.WriteLine("Was möchtest du tun?");
-Console.WriteLine("\t1. Neues Todo erstellen\n\t2. Todos anzeigen");
+
+string[] todos = new string[10];
+
+// das hier ist aktuell noch null!!!
+// Console.WriteLine(todos[9]);
 
 do
 {
+  Console.Clear();
+  Console.WriteLine("Was möchtest du tun?");
+  Console.WriteLine("\t1. Neues Todo erstellen\n\t2. Todos anzeigen\n\t3. Todo abhacken");
   var input = Console.ReadLine();
 
   switch (input)
   {
     case "1":
-      // Option 1: Todo erstellen
-      // Userintput nehmen
+      Console.WriteLine("Was hast du zu tun? <Enter zum bestätigen>");
       var todo = Console.ReadLine();
-      
-      // Neues Todo zur Todoliste hinzufügen
-      // Todo erstellen
-      Console.WriteLine("CREATE");
+      // Iterieren durch alle Todos (auch die leeren)
+      for (int i = 0; i < todos.Length; i++)
+      {
+        // Wenn das aktuelle Element im Array leer ist, dann...
+        if (todos[i] == null)
+        {
+          // Fügen wir das Todo hinzu
+          todos[i] = todo;
+          Console.WriteLine("Todo hinzugefügt: " + todos[i]);
+          break;
+        }
+      }
+
       break;
     case "2":
-      // Liste der Todos anzeigen
-      Console.WriteLine("READ");
+      Console.WriteLine("Du hast folgende Aufgaben:");
+      for (int i = 0; i < todos.Length; i++)
+      {
+        Console.WriteLine($"\t{i}. {todos[i]}");
+      }
+      Console.WriteLine("Drücke <ENTER> um zurück ins Menü zu kommen.");
+      Console.ReadKey();
+      break;
+    case "3":
+      // Löschen
+      Console.WriteLine("Welches Todo möchtest du löschen?");
+      for (int i = 0; i < todos.Length; i++)
+      {
+        Console.WriteLine($"\t{i}. {todos[i]}");
+      }
+      var todoIndexString = Console.ReadLine();
+      int todoIndex = int.Parse(todoIndexString);
+      // float todoIndex = float.Parse(todoIndexString);
+      // int todoIndex1 = Convert.ToInt32(todoIndexString);
+      todos[todoIndex] = null;
+      break;
+    case "4":
+      // Updaten/Verändern eines Todos (verändern des strings)
+      // 
       break;
   }
-} while (false);
+} while (true);
 
