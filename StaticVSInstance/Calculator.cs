3f241e95fc487ec7c9d4f7725af394406637535f
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2024-07-12 12:09:03 +0200

    TriangleArea und Circumference

--- StaticVSInstance/Calculator.cs
+++ StaticVSInstance/Calculator.cs
@@ -18,28 +18,36 @@
 
     /* Calculate()
     Refactoring -> Umschreiben vom Code zum Verbessern der Performance, Lesbarkeit oder auch Wartbarkeit */
-    public static int Calculate(int numberInput1, int numberInput2, Operator op) 
+    public static double Calculate(List<double> numberInputs, Operator op) 
     {
-      int result = 0;
+      double result = 0;
       Count++;
       Console.ForegroundColor = ConsoleColor.Magenta;
       switch (op)
       {
         case Operator.Add:
-          result = numberInput1 + numberInput2;
-          Console.WriteLine($"Das Ergebnis von {numberInput1} + {numberInput2} = {result}");
+          result = numberInputs[0] + numberInputs[1];
+          Console.WriteLine($"Das Ergebnis von {numberInputs[0]} + {numberInputs[1]} = {result}");
           break;
         case Operator.Subtract:
-          result = numberInput1 - numberInput2;
-          Console.WriteLine($"Das Ergebnis von {numberInput1} - {numberInput2} = {result}");
+          result = numberInputs[0] - numberInputs[1];
+          Console.WriteLine($"Das Ergebnis von {numberInputs[0]} - {numberInputs[1]} = {result}");
           break;
         case Operator.Multiply:
-          result = numberInput1 * numberInput2;
-          Console.WriteLine($"Das Ergebnis von {numberInput1} * {numberInput2} = {result}");
+          result = numberInputs[0] * numberInputs[1];
+          Console.WriteLine($"Das Ergebnis von {numberInputs[0]} * {numberInputs[1]} = {result}");
           break;
         case Operator.Divide:
-          result = numberInput1 / numberInput2;
-          Console.WriteLine($"Das Ergebnis von {numberInput1} / {numberInput2} = {result}");
+          result = numberInputs[0] / numberInputs[1];
+          Console.WriteLine($"Das Ergebnis von {numberInputs[0]} / {numberInputs[1]} = {result}");
+          break;
+        case Operator.TriangleArea:
+          result = TriangleArea(numberInputs[0], numberInputs[1]);
+          Console.WriteLine($"Die Fläche des Dreiecks [{numberInputs[0]}, {numberInputs[1]}] ist {result} groß.");
+          break;
+        case Operator.TriangleCircumference:
+          result = TriangleCircumference(numberInputs[0], numberInputs[1], numberInputs[2]);
+          Console.WriteLine($"Der Umfang des Dreiecks [{numberInputs[0]}, {numberInputs[1]}, {numberInputs[2]}] ist {result} groß.");
           break;
       }
       Console.ResetColor();
@@ -49,6 +57,12 @@
       return result;
     }
 
+    public static double TriangleArea(double baseSide, double height)
+      => (baseSide * height) / 2; // == 0.5 * base * height
+
+    public static double TriangleCircumference(double a, double b, double c) => a + b + c;
+
+    /*
     public static int Add(int numberInput1, int numberInput2)
     {
       Count++;
@@ -107,14 +121,18 @@
       return result;
     }
     */
-
-
   }
   public enum Operator
   {
     Add = 1,
-    Subtract = 2,
-    Multiply = 3,
-    Divide = 4,
+    Subtract,
+    Multiply,
+    Divide,
+    TriangleArea,
+    TriangleCircumference,
+    CircleArea,
+    CircleCircumference,
+    RectangleArea,
+    RectangleCircumference,
   }
 }
--- StaticVSInstance/Program.cs
+++ StaticVSInstance/Program.cs
@@ -1,8 +1,7 @@
 ﻿using StaticVSInstance;
 
-int numberInput1 = 0;
-int numberInput2 = 0;
-int result = 0;
+List<double> numberInputs = new();
+double result = 0;
 /*
 
 Calculator c = new Calculator();
@@ -43,12 +42,14 @@ var parsed = int.Parse("1");
 
 do
 {
+  numberInputs.Clear();
   Console.Clear();
   Console.WriteLine("Was willst du rechnen?");
-  Console.WriteLine("\t1.Add\n\t2.Subtract\n\t3.Multiply\n\t4.Divide");
+  Console.WriteLine("\t1.Add\n\t2.Subtract\n\t3.Multiply\n\t4.Divide\n\t5.Triangle Area\n\t6.Triagnle Circumference");
   var menuInput = Console.ReadLine();
-  numberInput1 = Calculator.GetNumberInput("Gebe die erste Zahl ein: ");
-  numberInput2 = Calculator.GetNumberInput("Gebe die zweite Zahl ein: ");
+  // numberInputs[10] = Calculator.GetNumberInput("Gebe die erste Zahl ein: "); geht nicht, weil nicht initialisiert. liste ist append only
+  numberInputs.Add(Calculator.GetNumberInput("Gebe die erste Zahl ein: "));
+  numberInputs.Add(Calculator.GetNumberInput("Gebe die zweite Zahl ein: "));
   int menuInputIndex = int.Parse(menuInput);
   Operator op = (Operator)menuInputIndex;
   switch (op)
@@ -56,16 +57,23 @@ do
     case Operator.Add:
       //Calculator.
       // var numberInput1 = Calculator.GetInput();
-      Calculator.Calculate(numberInput1, numberInput2, Operator.Add);
+      Calculator.Calculate(numberInputs, Operator.Add);
       break;
     case Operator.Subtract:
-      Calculator.Calculate(numberInput1, numberInput2, Operator.Subtract);
+      Calculator.Calculate(numberInputs, Operator.Subtract);
       break;
     case Operator.Multiply:
-      Calculator.Calculate(numberInput1, numberInput2, Operator.Multiply);
+      Calculator.Calculate(numberInputs, Operator.Multiply);
       break;
     case Operator.Divide:
-      Calculator.Calculate(numberInput1, numberInput2, Operator.Divide);
+      Calculator.Calculate(numberInputs, Operator.Divide);
+      break;
+    case Operator.TriangleArea:
+      Calculator.Calculate(numberInputs, Operator.TriangleArea);
+      break;
+    case Operator.TriangleCircumference:
+      numberInputs.Add(Calculator.GetNumberInput("Gebe die dritte Zahl ein: "));
+      Calculator.Calculate(numberInputs, Operator.TriangleCircumference);
       break;
   }
 
